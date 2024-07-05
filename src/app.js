9bc3b3c3c0bf92416b1b040c9d27ef9600d5b628
bharat2468 <129318600+bharat2468@users.noreply.github.com>
2024-07-05 16:34:34 +0530

    file setup for project completed

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
+
 
 const app = express();
 
+//? CORS config
 const corsOptions = {
 	origin: process.env.ALLOWED_ORIGINS,
 	credentials: true,
 };
 app.use(cors(corsOptions));
+
+//? cookie parser config to read cookies 
 app.use(cookieParser());
 
+//? config for data recieved in the requests 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+// !routes import 
+import userRoutes from "./routes/user.routes.js"
+
+
+// !routes declare 
+app.use("/api/v1/users",userRoutes);
+
 
 export { app };
